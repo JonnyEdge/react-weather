c5938778a67784db223a05b950e652cb871592e3
JonnyEdge <jonnyedge@outlook.com>
2018-10-20 16:03:57 +0100

    Went back to fixed forecast-summary component jest tests.

--- __tests__/components/forecast-summary.test.jsx
+++ __tests__/components/forecast-summary.test.jsx
@@ -2,13 +2,42 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import ForecastSummary from '../../src/components/forecast-summary';
 
-it('renders the passed date in a P tag', () => {
+it('renders the passed date in its own div', () => {
   const wrapper = Enzyme.shallow((
     <ForecastSummary date="foo" />
   ));
 
-  // const text = wrapper.find('p').text();
-  const text = wrapper.find('.forecast-summary__date');
+  const text = wrapper.find('.date').text();
 
   expect(text).toEqual('foo');
+});
+
+it('renders the passed temperature in its own div', () => {
+  const wrapper = Enzyme.shallow((
+    <ForecastSummary temperature="20" />
+  ));
+
+  const text = wrapper.find('.temperature').text();
+
+  expect(text).toEqual('20');
+});
+
+it('renders the passed description in its own div', () => {
+  const wrapper = Enzyme.shallow((
+    <ForecastSummary description="bar" />
+  ));
+
+  const text = wrapper.find('.description').text();
+
+  expect(text).toEqual('bar');
+});
+
+it('renders the passed icon in its own div', () => {
+  const wrapper = Enzyme.shallow((
+    <ForecastSummary date="foobar" />
+  ));
+
+  const text = wrapper.find('.icon').text();
+
+  expect(text).toEqual('foobar');
 });
--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -4,22 +4,16 @@ import PropTypes from 'prop-types';
 const ForecastSummary = props => (
 
   <span>
-    <div className="date">{props.date}</div>
-  
-  
+    <div className="date">{props.date}</div> 
     <div className="temperature">{props.temperature}</div>
-  
- 
     <div className="description">{props.description}</div>
-  
- 
     <div className="icon">{props.icon}</div>
   </span>
 );
 
 ForecastSummary.propTypes = {
-  date: PropTypes.number,
-  temperature: PropTypes.number,
+  date: PropTypes.string,
+  temperature: PropTypes.string,
   description: PropTypes.string,
   icon: PropTypes.string,
 };
