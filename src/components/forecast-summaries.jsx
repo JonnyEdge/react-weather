86940061b21615fdb324532af8eb9dcf70156ae9
Jonny Edge <jonnyedge@outlook.com>
2018-10-24 20:11:23 +0100

    Fixed issue with function attempting to be called on the event not being read as a function. More Details button now functions as intended.

--- src/components/forecast-summaries.jsx
+++ src/components/forecast-summaries.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ForecastSummary from './forecast-summary';
 import '../styles/forecast-summaries.scss';
 
+// Put "console.log(props) ||"" after => and before ( in the constructor function to get a readout of what props contains
+
 const ForecastSummaries = props => (
   <div className="forecast-summaries">
     {
       props.forecasts.map(forecast => (
         <ForecastSummary
-          onSelect={forecast.onForecastSelect}
+          onSelect={props.onForecastSelect}
           key={forecast.date}
           date={forecast.date}
           temperature={forecast.temperature.max}
